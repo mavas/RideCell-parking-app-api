6c0af43278a4973398d01c59904f05a7f1f14e4f
mavas <10604882+mavas@users.noreply.github.com>
2017-04-03 15:28:19 -0700

    Got database working.

--- server.py
+++ server.py
@@ -7,6 +7,7 @@ from flask import jsonify, request
 app = flask.Flask(__name__)
 DATABASE = 'db.sqlite'
 if not os.path.exists(DATABASE):
+    print("Creating database.")
     c = sqlite3.connect('db.sqlite')
     c.execute('CREATE TABLE parking_spots (lng, lat, reserved)')
     c.execute('INSERT INTO parking_spots VALUES (2, 4, 0)')
@@ -19,13 +20,31 @@ if not os.path.exists(DATABASE):
 def list_available():
     r = []
     d = request.values.to_dict()
+    c = sqlite3.connect(DATABASE).cursor()
+
     if 'lng' in d and 'lat' in d:
-        lng = d['lng']
-        lat = d['lat']
-        c = sqlite3.connect(DATABASE)
-        c.execute('SELECT * from parking_spots where lng = ? and lat = ?', lng, lat)
+        lng = int(d['lng'])
+        lat = int(d['lat'])
+        q = 'SELECT * from parking_spots where lng = ? and lat = ?'
+        c.execute(q, (lng, lat))
         for i in c.fetchall():
             r.append(i)
+
+    elif 'lng' in d and not 'lat' in d:
+        lng = int(d['lng'])
+        q = 'SELECT * from parking_spots where lng = ?'
+        c.execute(q, (lng,))
+        for i in c.fetchall():
+            r.append(i)
+
+    elif not 'lng' in d and 'lat' in d:
+        lat = int(d['lat'])
+        q = 'SELECT * from parking_spots where lat = ?'
+        c.execute(q, (lat,))
+        for i in c.fetchall():
+            r.append(i)
+
+    c.close()
     return jsonify({'available': r})
 
 
